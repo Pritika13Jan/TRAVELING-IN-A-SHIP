fcced6ec1573ce72b04b5dd695c132beaddf1963
Pritika13Jan <86922603+Pritika13Jan@users.noreply.github.com>
2021-08-03 18:25:48 +0530

    Add files via upload

---
+++ Travelling in the Ship.js
@@ -0,0 +1,20 @@
+var seaImg,shipImg1,shipImg2,shipImg3,shipImg4
+
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadImage("ship1.png");
+shipImg2 = loadImage("ship2.png");
+shipImg3 = loadImage("ship3.png");
+shipImg4 = loadImage("ship4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
